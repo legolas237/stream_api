15246b1993e69564caaa8773541dfafaff39b412
h.njimbong <hyacinthenjimbong18@gmail.com>
2021-11-24 06:05:12 -0800

    Add user and user_detail table

--- app/Helpers.php
+++ app/Helpers.php
@@ -1,6 +1,14 @@
 <?php
 
 if (!function_exists('api_response')) {
+    /**
+     * Generic response format
+     *
+     * @param int $code
+     * @param string|null $message
+     * @param null $data
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
     function api_response(int $code, string $message = null, $data = null)
     {
         return response([
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -6,6 +6,8 @@ use App\Services\Platform\CountryService;
 use Giggsey\Locale\Locale;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use PragmaRX\Countries\Package\Support\Collection as PragmaRXCollection;
+use PragmaRX\Countries\Package\Countries;
 
 class Country extends Model
 {
@@ -41,11 +43,35 @@ class Country extends Model
     protected $casts = [];
 
     /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['native_name'];
+
+    /**
      * The attributes that should be mutated to dates.
      *
      * @var array
      */
     protected $dates = ['created_at', 'updated_at', 'deleted_at', 'activated_at'];
+
+    /**
+     * Determine if the user is an administrator.
+     *
+     * @return string
+     */
+    public function getNativeNameAttribute()
+    {
+        $countries = new Countries();
+        $country = $countries->where('cca2', strtoupper($this->attributes['alpha_code']))->first();
+
+        if(! is_null($country) && $country->contains('name')) {
+            return $this->getNativeName($this->attributes['designation'], $country);
+        }
+
+        return $this->attributes['designation'];
+    }
 
     /**
      * The "booting" method of the model.
@@ -65,5 +91,40 @@ class Country extends Model
             $country->{'designation'} = $country->getOriginal('designation');
             $country->{'designation'} = $localeDesignation;
         });
+    }
+
+    /**
+     * Custom functions
+     */
+
+    /**
+     * Get native country name
+     *
+     * @param string $designation
+     * @param PragmaRXCollection $country
+     * @return mixed
+     */
+    public function getNativeName(string $designation, PragmaRXCollection $country)
+    {
+        $commonName = $country->name->common;
+        $languages = $country->languages ?? collect();
+        $language = $languages->keys()->first() ?? null;
+        $nativeNames = $country->name->native ?? null;
+
+        if (filled($language) && filled($nativeNames) && filled($nativeNames[$language]) ?? null) {
+            $native = $nativeNames[$language]['common'] ?? null;
+        }
+
+        if (blank($native ?? null) && filled($nativeNames)) {
+            $native = $nativeNames->first()['common'] ?? null;
+        }
+
+        $native = $native ?? $commonName;
+
+        if ($native !== $commonName && filled($native)) {
+            return $native;
+        }
+
+        return $designation;
     }
 }
